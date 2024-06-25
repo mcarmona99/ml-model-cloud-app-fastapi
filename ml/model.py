f66b85336ed7dc3b1b45ddb3a95f0ff407f3a5c3
MANUEL CARMONA PEREZ <manuel.carmona-perez@t-systems.com>
2024-06-25 20:45:16 +0200

    Add function to evaluate data on slices

--- ml/model.py
+++ ml/model.py
@@ -1,8 +1,11 @@
+import json
 import os
 import pickle
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+from ml.data import process_data
 
 
 # Optional: implement hyperparameter tuning.
@@ -18,7 +21,7 @@ def train_model(X_train, y_train):
         Labels.
     Returns
     -------
-    model
+    model : LogisticRegression
         Trained machine learning model.
     """
     model = LogisticRegression(max_iter=1000)
@@ -42,7 +45,7 @@ def save_model_pickle(model, output_path=os.path.join('model', 'model.pkl')):
         pickle.dump(model, model_file)
 
 
-def compute_model_metrics(y, preds):
+def compute_model_metrics(y, preds, log=True):
     """
     Validates the trained machine learning model using precision, recall, and F1.
 
@@ -52,6 +55,8 @@ def compute_model_metrics(y, preds):
         Known labels, binarized.
     preds : np.array
         Predicted labels, binarized.
+    log : bool
+        Whether to log metrics or not.
     Returns
     -------
     precision : float
@@ -61,15 +66,19 @@ def compute_model_metrics(y, preds):
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
+    if log:
+        print(f"Precision: {precision}")
+        print(f"Recall: {recall}")
+        print(f"Fbeta: {fbeta}")
     return precision, recall, fbeta
 
 
 def inference(model, X):
-    """ Run model inferences and return the predictions.
+    """Run model inferences and return the predictions.
 
-    Inputs
+    Inputs:
     ------
-    model : ???
+    model : LogisticRegression
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -78,4 +87,45 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    y_pred = model.predict(X)
+    return y_pred
+
+
+def evaluate_slice(model, data, cat_features, encoder, lb, output_path=os.path.join('model', 'slice_evaluation.json')):
+    """Evaluate the model on slices of dataset.
+
+    Inputs:
+    -------
+    model : LogisticRegression
+        Trained machine learning model.
+    data : pd.DataFrame
+        Pandas DataFrame containing the dataset.
+    cat_features : list
+        List of categorical feature names.
+    output_path : str
+        Path to save the evaluation results in JSON format. Defaults to 'model/slice_evaluation.json'.
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained OneHotEncoder if training is True, otherwise returns the encoder passed in.
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained LabelBinarizer if training is True, otherwise returns the binarizer passed in.
+    """
+
+    slice_evaluation = []
+
+    for feature in cat_features:
+        for category in data[feature].unique():
+            slice = data[data[feature] == category]
+
+            X_test, y_test, _, _ = (
+                process_data(slice, categorical_features=cat_features, label='salary', training=False,
+                             encoder=encoder, lb=lb))
+
+            y_pred = inference(model, X_test)
+
+            precision, recall, fbeta = compute_model_metrics(y_test, y_pred, log=False)
+            slice_evaluation.append(
+                {'feature': feature, 'category': category, 'precision': precision, 'recall': recall, 'fbeta': fbeta})
+
+    print(slice_evaluation)
+    with open(output_path, 'w') as f:
+        json.dump(slice_evaluation, f, indent=4)
