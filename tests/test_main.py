aebcbfee7a2520d8509f3f6011b259aa10c0215e
MANUEL CARMONA PEREZ <manuel.carmona-perez@t-systems.com>
2024-07-03 10:12:08 +0200

    Move API unit tests to tests folder

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,11 @@
+import os
+import sys
 from unittest.mock import patch
 
 import pytest
 from fastapi.testclient import TestClient
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from main import app
 
