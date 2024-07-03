d734d6f687f2e5423c5a1716dce126e0b204f719
MANUEL CARMONA PEREZ <manuel.carmona-perez@t-systems.com>
2024-07-03 12:53:53 +0200

    Update InferenceBody model example

--- main.py
+++ main.py
@@ -57,25 +57,28 @@ class InferenceBody(BaseModel):
     hours_per_week: int = Field(..., alias="hours-per-week")
     native_country: str = Field(..., alias="native-country")
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "age": 36,
-                "workclass": "Private",
-                "fnlgt": 484024,
-                "education": "HS-grad",
-                "education-num": 9,
-                "marital-status": "Divorced",
-                "occupation": "Machine-op-inspct",
-                "relationship": "Unmarried",
-                "race": "White",
-                "sex": "Male",
-                "capital-gain": 0,
-                "capital-loss": 0,
-                "hours-per-week": 40,
-                "native-country": "United-States",
-            }
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "age": 36,
+                    "workclass": "Private",
+                    "fnlgt": 484024,
+                    "education": "HS-grad",
+                    "education-num": 9,
+                    "marital-status": "Divorced",
+                    "occupation": "Machine-op-inspct",
+                    "relationship": "Unmarried",
+                    "race": "White",
+                    "sex": "Male",
+                    "capital-gain": 0,
+                    "capital-loss": 0,
+                    "hours-per-week": 40,
+                    "native-country": "United-States",
+                }
+            ]
         }
+    }
 
 
 @app.post("/predict")
