c9e46ccfc0ee718f2ae9ec931c15bf3a4acde2fc
MANUEL CARMONA PEREZ <manuel.carmona-perez@t-systems.com>
2024-07-04 17:45:27 +0200

    Update predict API test

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,14 +21,17 @@ def test_welcome():
 valid_test_cases = [
     ({"age": 36, "workclass": "Private", "fnlgt": 484024, "education": "HS-grad", "education-num": 9,
       "marital-status": "Divorced", "occupation": "Machine-op-inspct", "relationship": "Unmarried", "race": "White",
-      "sex": "Male", "capital-gain": 0, "capital-loss": 0, "hours-per-week": 40, "native-country": "United-States"}),
+      "sex": "Male", "capital-gain": 0, "capital-loss": 0, "hours-per-week": 40, "native-country": "United-States"},
+     [0]),  # Test case for prediction: <=50K
     ({"age": 44, "workclass": "Private", "fnlgt": 75227, "education": "Prof-school", "education-num": 15,
       "marital-status": "Never-married", "occupation": "Prof-specialty", "relationship": "Not-in-family",
       "race": "White", "sex": "Male", "capital-gain": 14084, "capital-loss": 0, "hours-per-week": 40,
-      "native-country": "United-States"}),
+      "native-country": "United-States"},
+     [1]),  # Test case for prediction: >50K
     ({"age": 62, "workclass": "Private", "fnlgt": 664366, "education": "Bachelors", "education-num": 13,
       "marital-status": "Married-civ-spouse", "occupation": "Sales", "relationship": "Husband", "race": "White",
-      "sex": "Male", "capital-gain": 0, "capital-loss": 0, "hours-per-week": 60, "native-country": "United-States"}),
+      "sex": "Male", "capital-gain": 0, "capital-loss": 0, "hours-per-week": 60, "native-country": "United-States"},
+     [0]),  # Test case for prediction: <=50K
 ]
 
 invalid_test_cases = [
@@ -44,11 +47,12 @@ invalid_test_cases = [
 ]
 
 
-@pytest.mark.parametrize("input_data", valid_test_cases)
-def test_predict(input_data):
+@pytest.mark.parametrize("input_data, expected_prediction", valid_test_cases)
+def test_predict(input_data, expected_prediction):
     response = test_client.post("/predict", json=input_data)
     assert response.status_code == 200
     assert "prediction" in response.json()
+    assert response.json()['prediction'] == expected_prediction
     assert "input" in response.json()
     assert type(response.json()['input']) == dict
     assert type(response.json()['prediction']) == list
